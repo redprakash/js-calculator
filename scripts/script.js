81378c289fdbc24443a63b7bb625a439a94e9ffb
Prakash Poudel <bhaktpoudel@gmail.com>
2021-09-27 23:10:35 +1000

    Inital setup

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// const plus = document.querySelector('.add');
+// const displayOperator = document.querySelector('.display__operator');
+// const leftOperand = document.querySelector('.display__leftoperand');
+// plus.addEventListener('click', add);
+// function add() {
+//   if (plus.innerHTML === '+') {
+//     displayOperator.textContent = '+';
+//   }
+// }
+// const seven = document.querySelector('.seven');
+// seven.addEventListener('click', () => {
+//   leftOperand.textContent = leftOperand.innerHTML + '7';
+// });
