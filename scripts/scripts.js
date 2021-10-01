50ef17f82568faadd273dd321f28afca185b8eef
Prakash Poudel <bhaktpoudel@gmail.com>
2021-10-01 18:13:03 +1000

    Basic operations working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,23 +4,8 @@ let calculatorDisplay = document.querySelector('.display__userInput');
 
 let pressedNum = document.querySelectorAll('button');
 
-for (let i = 0; i < pressedNum.length; i++) {
-  pressedNum[i].addEventListener('click', () => {
-    calculatorDisplay.textContent += pressedNum[i].textContent;
-
-    if (/[-+/x]/.test(calculatorDisplay.textContent)) {
-      filteredDisplayNum = calculatorDisplay.textContent.slice(0, -1);
-      myOperator = calculatorDisplay.textContent.slice(-1);
-
-      userNumberOne = filteredDisplayNum;
-
-      calculatorDisplay.textContent = '';
-    }
-  });
-}
-
 //Function to do the calculation
-function calculate(numberOne, operator, numberTwo) {
+let result = function calculate(numberOne, operator, numberTwo) {
   switch (operator) {
     case '+': {
       return parseFloat(numberOne) + parseFloat(numberTwo);
@@ -37,7 +22,22 @@ function calculate(numberOne, operator, numberTwo) {
     default:
       alert('Sorry invalid operator or something is wrong');
   }
+};
+
+for (let i = 0; i < pressedNum.length; i++) {
+  pressedNum[i].addEventListener('click', () => {
+    calculatorDisplay.textContent += pressedNum[i].textContent;
+    if (userNumberOne) {
+      userNumberTwo = calculatorDisplay.textContent;
+    } else if (/[-+/x]/.test(calculatorDisplay.textContent)) {
+      filteredDisplayNum = calculatorDisplay.textContent.slice(0, -1);
+      myOperator = calculatorDisplay.textContent.slice(-1);
+      userNumberOne = filteredDisplayNum;
+      calculatorDisplay.textContent = '';
+    }
+  });
 }
+
 //Function to clear the display when AC pressed
 const allClear = document.querySelector('.clear');
 allClear.addEventListener('click', clearDisplay);
@@ -48,6 +48,9 @@ function clearDisplay() {
 //Function to display result when = sign pressed
 const equal = document.querySelector('.equal');
 equal.addEventListener('click', () => {
-  let result = calculate(userNumberOne, myOperator, userNumberTwo);
-  calculatorDisplay.textContent = result;
+  calculatorDisplay.textContent = result(
+    userNumberOne,
+    myOperator,
+    userNumberTwo
+  );
 });
